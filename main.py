b25ac039aa3b8ac542a26f6bdc69bc77ad5598e6
Keith Yao <78155676+AgentKettlepot@users.noreply.github.com>
2024-07-22 16:52:25 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,283 @@
+# A project dedicated to my glorious king the great Wardell Steph Curry
+# Comparing Steph Curry to other players from 2012-18
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests 
+from bs4 import BeautifulSoup
+import regex as re
+whole_df = pd.read_csv("archive\\2012-18_playerBoxScore.csv")
+#print(whole_df.describe())
+
+def getPlayerStats(df, firstName, lastName, teamAbbr, year):
+    df['gmDate'] = pd.to_datetime(df['gmDate'])
+    df['year'] = df['gmDate'].dt.year
+    player_df = df.loc[(df['playLNm'] == lastName) & (df["playFNm"] == firstName) & (df["year"].isin(year))]
+    #print(player_df)
+    return player_df
+
+def getPositionStats(df, position, year):
+    df['gmDate'] = pd.to_datetime(df['gmDate'])
+    df['year'] = df['gmDate'].dt.year
+    position_df = df.loc[(df['playPos'] == position) & (df["year"].isin(year))]
+    #print(position_df)
+    return position_df
+
+def getWeightHeightStats(df, weight, height, dw, dh, year):
+    df['gmDate'] = pd.to_datetime(df['gmDate'])
+    df['year'] = df['gmDate'].dt.year
+    wh_df = df.loc[(df['playWeight'].isin([weight-dw, weight+dw])) & (df['playHeight'].isin([height-dh, height+dh])) & (df["year"].isin(year))]
+    #print(wh_df)
+    return wh_df
+
+def getAttrPerMin(df, attr):
+    df['fullName'] = df['playFNm'] + ' ' + df['playLNm']
+    df['attrPerMin'] = df[attr] / df['playMin']
+    grouped_df = df.groupby('fullName', as_index=False)['attrPerMin'].mean()
+    #print(attribute)
+    return grouped_df
+
+def getAttrPerGame(df, attr):
+    df['fullName'] = df['playFNm'] + ' ' + df['playLNm']
+    df['attrPerGame'] = df[attr]
+    grouped_df = df.groupby('fullName', as_index=False)["attrPerGame"].mean()
+    return grouped_df
+
+# Displays a certain attribute from 2 dataframes in a barchart
+def displayPerMinute(df1, df2, attribute, title, unit):
+    plt.figure(figsize=(12, 6))
+
+    if unit == "min":
+        curry_df = getAttrPerMin(df1, attribute).sort_values(by='attrPerMin').dropna()
+        curry_df = curry_df[curry_df['attrPerMin'] != 0]
+        
+        pg_df = getAttrPerMin(df2, attribute).sort_values(by='attrPerMin').dropna()
+        pg_df = pg_df[pg_df['attrPerMin'] != 0]
+    
+        combined_df = pd.concat([pg_df, curry_df]).sort_values(by='attrPerMin')
+
+        plt.bar(combined_df['fullName'], combined_df['attrPerMin'], color='skyblue', label='PG Data')
+        plt.bar(curry_df['fullName'], curry_df['attrPerMin'], color='black', label='Curry Data')
+    else:
+        curry_df = getAttrPerGame(df1, attribute).sort_values(by='attrPerGame').dropna()
+        curry_df = curry_df[curry_df['attrPerGame'] != 0]
+        
+        pg_df = getAttrPerGame(df2, attribute).sort_values(by='attrPerGame').dropna()
+        pg_df = pg_df[pg_df['attrPerGame'] != 0]
+        
+        combined_df = pd.concat([pg_df, curry_df]).sort_values(by='attrPerGame')
+        plt.bar(combined_df['fullName'], combined_df['attrPerGame'], color='skyblue', label='PG Data')
+        plt.bar(curry_df['fullName'], curry_df['attrPerGame'], color='black', label='Curry Data')
+
+    
+    plt.xlabel('Player Name')
+    plt.ylabel('Count')
+    plt.title(title)
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+'''
+teamRslt - win/loss
+playMin - minutes played
+playPos - player position
+playHeight - height
+playWeight - weight
+playBDate - birthday
+playPTS
+playAST
+playTO
+playSTL
+playBLK
+playPF
+playFGA
+playFGM
+playFG%
+play2PA
+play2PM
+play2P%
+play3PA
+play3PM
+play3p%
+playFTA
+playFTM
+playFT%
+playORB
+playDRB
+playTRB
+'''
+### COMPARING TO OTHER PGs ###
+curry = getPlayerStats(whole_df, "Wardell", "Curry", "GS", [2015])
+
+def pgCompare(years):
+    pg = getPositionStats(whole_df,"PG", years)
+    # Getting the points per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playPTS", "Curry vs Point Guards, 2015, Points Per Min", "min")
+    # Getting the assists per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playAST", "Curry vs Point Guards, 2015, Assists Per Min", "min")
+    # Getting the turnovers per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playTO", "Curry vs Point Guards, 2015, Turnovers Per Min", "min")
+    # Getting the steals per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playSTL", "Curry vs Point Guards, 2015, Steals Per Min", "min")
+    # Getting the blocks per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playBLK", "Curry vs Point Guards, 2015, Blocks Per Min", "min")
+
+    # Getting the personal fouls per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playPF", "Curry vs Point Guards, 2015, Personal Fouls Per Min", "min")
+
+    # Getting the FGA per game in the 2015 season--------------------------------
+    displayPerMinute(curry, pg, "playFGA", "Curry vs Point Guards, 2015, FGA Per Min", "min")
+
+    displayPerMinute(curry, pg, "playFGM", "Curry vs Point Guards, 2015, FGM Per Min", "min")
+    displayPerMinute(curry, pg, "playFG%", "Curry vs Point Guards, 2015, FG% Per Game", "game")
+
+    displayPerMinute(curry, pg, "play2PA", "Curry vs Point Guards, 2015, 2PA Per Min", "min")
+    displayPerMinute(curry, pg, "play2PM", "Curry vs Point Guards, 2015, 2PM Per Min", "min")
+    displayPerMinute(curry, pg, "play2P%", "Curry vs Point Guards, 2015, 2P% Per Game", "game")
+
+    displayPerMinute(curry, pg, "play3PA", "Curry vs Point Guards, 2015, 3PA Per Min", "min")
+    displayPerMinute(curry, pg, "play3PM", "Curry vs Point Guards, 2015, 3PM Per Min", "min")
+    displayPerMinute(curry, pg, "play3P%", "Curry vs Point Guards, 2015, 3P% Per Game", "game")
+
+    displayPerMinute(curry, pg, "playFTA", "Curry vs Point Guards, 2015, FTA Per Min", "min")
+    displayPerMinute(curry, pg, "playFTM", "Curry vs Point Guards, 2015, FTM Per Min", "min")
+    displayPerMinute(curry, pg, "playFT%", "Curry vs Point Guards, 2015, FT% Per Game", "game")
+
+    displayPerMinute(curry, pg, "playORB", "Curry vs Point Guards, 2015, ORB Per Min", "min")
+    displayPerMinute(curry, pg, "playDRB", "Curry vs Point Guards, 2015, DRB Per Min", "min")
+    displayPerMinute(curry, pg, "playTRB", "Curry vs Point Guards, 2015, TRB Per Min", "min")
+pgCompare([2015])
+
+### COMPARING TO SIMILAR WEIGHT/HEIGHT ###
+
+def whCompare(years):
+    wh_df = getWeightHeightStats(whole_df,185, 75, 5, 2, years)
+    # Getting the points per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playPTS", "Curry vs Similar Builds, 2015, Points Per Min", "min")
+    # Getting the assists per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playAST", "Curry vs Similar Builds, 2015, Assists Per Min", "min")
+    # Getting the turnovers per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playTO", "Curry vs Similar Builds, 2015, Turnovers Per Min", "min")
+    # Getting the steals per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playSTL", "Curry vs Similar Builds, 2015, Steals Per Min", "min")
+    # Getting the blocks per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playBLK", "Curry vs Similar Builds, 2015, Blocks Per Min", "min")
+
+    # Getting the personal fouls per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playPF", "Curry vs Similar Builds, 2015, Personal Fouls Per Min", "min")
+
+    # Getting the FGA per game in the 2015 season--------------------------------
+    displayPerMinute(curry, wh_df, "playFGA", "Curry vs Similar Builds, 2015, FGA Per Min", "min")
+
+    displayPerMinute(curry, wh_df, "playFGM", "Curry vs Similar Builds, 2015, FGM Per Min", "min")
+    displayPerMinute(curry, wh_df, "playFG%", "Curry vs Similar Builds, 2015, FG% Per Game", "game")
+
+    displayPerMinute(curry, wh_df, "play2PA", "Curry vs Similar Builds, 2015, 2PA Per Min", "min")
+    displayPerMinute(curry, wh_df, "play2PM", "Curry vs Similar Builds, 2015, 2PM Per Min", "min")
+    displayPerMinute(curry, wh_df, "play2P%", "Curry vs Similar Builds, 2015, 2P% Per Game", "game")
+
+    displayPerMinute(curry, wh_df, "play3PA", "Curry vs Similar Builds, 2015, 3PA Per Min", "min")
+    displayPerMinute(curry, wh_df, "play3PM", "Curry vs Similar Builds, 2015, 3PM Per Min", "min")
+    displayPerMinute(curry, wh_df, "play3P%", "Curry vs Similar Builds, 2015, 3P% Per Game", "game")
+
+    displayPerMinute(curry, wh_df, "playFTA", "Curry vs Similar Builds, 2015, FTA Per Min", "min")
+    displayPerMinute(curry, wh_df, "playFTM", "Curry vs Similar Builds, 2015, FTM Per Min", "min")
+    displayPerMinute(curry, wh_df, "playFT%", "Curry vs Similar Builds, 2015, FT% Per Game", "game")
+
+    displayPerMinute(curry, wh_df, "playORB", "Curry vs Similar Builds, 2015, ORB Per Min", "min")
+    displayPerMinute(curry, wh_df, "playDRB", "Curry vs Similar Builds, 2015, DRB Per Min", "min")
+    displayPerMinute(curry, wh_df, "playTRB", "Curry vs Similar Builds, 2015, TRB Per Min", "min")
+#whCompare([2015])
+
+### COMPARING TO OTHER ALL STARS OF THE YEAR
+def getAllStars(year):
+    url = f'https://www.basketball-reference.com/leagues/NBA_{year}.html#all_all_star_game_rosters'
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Find the All-Star Game Rosters section
+        all_star_rosters = soup.find(id='all_all_star_game_rosters')
+        raw = str(all_star_rosters)
+        raw = str(re.sub('<[^<]+?>', '', str(all_star_rosters)))
+        raw = raw.replace("&nbsp;", ", ").replace("*", "").replace(", -->", "").replace("All-Star Game Rosters", "").replace(", ,", ", ")
+        raw = " ".join(raw.split())
+        #print(all_star_rosters.prettify())\
+        #print(raw)
+    else:
+        print('Failed to retrieve data', response.status_code)
+    list_of_players = raw.split(", ")
+    #print(list_of_players)
+    allStars = []
+    for ele in list_of_players:
+        if all(c.isalpha() or c.isspace() for c in ele.replace(" ", "")) and ele != "West " and len(ele)>=2:
+            #print(ele)
+            allStars.append(ele)
+    return allStars
+
+
+def compareAllStars(df2):
+    # Getting the points per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playPTS", "Curry vs All Stars, 2015, Points Per Min", "min")
+    # Getting the assists per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playAST", "Curry vs All Stars, 2015, Assists Per Min", "min")
+    # Getting the turnovers per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playTO", "Curry vs All Stars, 2015, Turnovers Per Min", "min")
+    # Getting the steals per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playSTL", "Curry vs All Stars, 2015, Steals Per Min", "min")
+    # Getting the blocks per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playBLK", "Curry vs All Stars, 2015, Blocks Per Min", "min")
+
+    # Getting the personal fouls per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playPF", "Curry vs All Stars, 2015, Personal Fouls Per Min", "min")
+
+    # Getting the FGA per game in the 2015 season--------------------------------
+    displayPerMinute(curry, df2, "playFGA", "Curry vs All Stars, 2015, FGA Per Min", "min")
+
+    displayPerMinute(curry, df2, "playFGM", "Curry vs All Stars, 2015, FGM Per Min", "min")
+    displayPerMinute(curry, df2, "playFG%", "Curry vs All Stars, 2015, FG% Per Game", "game")
+
+    displayPerMinute(curry, df2, "play2PA", "Curry vs All Stars, 2015, 2PA Per Min", "min")
+    displayPerMinute(curry, df2, "play2PM", "Curry vs All Stars, 2015, 2PM Per Min", "min")
+    displayPerMinute(curry, df2, "play2P%", "Curry vs All Stars, 2015, 2P% Per Game", "game")
+
+    displayPerMinute(curry, df2, "play3PA", "Curry vs All Stars, 2015, 3PA Per Min", "min")
+    displayPerMinute(curry, df2, "play3PM", "Curry vs All Stars, 2015, 3PM Per Min", "min")
+    displayPerMinute(curry, df2, "play3P%", "Curry vs All Stars, 2015, 3P% Per Game", "game")
+
+    displayPerMinute(curry, df2, "playFTA", "Curry vs All Stars, 2015, FTA Per Min", "min")
+    displayPerMinute(curry, df2, "playFTM", "Curry vs All Stars, 2015, FTM Per Min", "min")
+    displayPerMinute(curry, df2, "playFT%", "Curry vs All Stars, 2015, FT% Per Game", "game")
+
+    displayPerMinute(curry, df2, "playORB", "Curry vs All Stars, 2015, ORB Per Min", "min")
+    displayPerMinute(curry, df2, "playDRB", "Curry vs All Stars, 2015, DRB Per Min", "min")
+    displayPerMinute(curry, df2, "playTRB", "Curry vs All Stars, 2015, TRB Per Min", "min")
+
+def getStars(years):
+    list_of_stars = []
+    for year in years:
+        list_of_stars += getAllStars(year)
+
+    df_list = []
+    print(list_of_stars)
+
+    for star in list_of_stars:
+        first_name, last_name = star.split()[0], star.split()[1]
+        print("ALL STAR NAME: ", first_name, last_name)
+        
+        player_stats = getPlayerStats(whole_df, first_name, last_name, "", years)
+        player_stats_df = player_stats if isinstance(player_stats, pd.DataFrame) else pd.DataFrame()
+        
+        df_list.append(player_stats_df)
+
+    # Concatenate all DataFrames in the list into a single DataFrame
+    all_star_df = pd.concat(df_list, ignore_index=True)
+
+    print(all_star_df)
+    return all_star_df
+
+#all_stars = getStars([2017, 2018])
+#compareAllStars(all_stars)
+
+### INTERACTIVE VERSION ON WEB ###
---
+++ sum_player.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import regex as re
+
+whole_df = pd.read_csv("archive\\2012-18_playerBoxScore.csv")
+whole_df["year"] = whole_df["gmDate"].str.slice(0, 4)
+whole_df["month"] = whole_df["gmDate"].str.slice(5, 7)
+# Returns a new df with year | player name | total pts | total assists ...
+def agg_stats (df):
+    aggregated_df = pd.DataFrame()
+    aggregated_df = df.groupby(["year", "month", "playDispNm"]).agg({
+        "playPTS" : "sum",
+        "playAST" : "sum",
+        "playTO"  : "sum",
+        "playSTL" : "sum",
+        "playBLK" : "sum",
+        "playFGM"  : "sum",     
+        "play2PA" : "sum",
+        "play2PM" : "sum",
+        "play3PA"  : "sum",   
+        "play3PM" : "sum",
+        "playFTA"  : "sum",          
+        "playFTM"  : "sum",    
+    }).reset_index()
+    aggregated_df.columns = ["year", "month", "player_name", "playPTS", "playAST", 
+                             "playTO", "playSTL", "playBLK", "playFGM", "play2PA", 
+                             "play2PM", "play3PA", "play3PM", "playFTA", "playFTM"]
+    print(aggregated_df)
+    return aggregated_df
+aggregated_data = agg_stats(whole_df)
+
+aggregated_data.to_csv("aggregated_data_by_year.csv")
